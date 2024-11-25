d61110c5a1c91b7cacad0928e7f6a7a8d296baed
ivan_yi <88026351+ivanyi031@users.noreply.github.com>
2024-11-25 14:58:47 +0800

    Add files via upload

---
+++ encoder_interrupt_test/Calibration.cpp
@@ -0,0 +1,139 @@
+#include "Calibration.h"
+#include  "SpiEncoder.h"
+
+
+void calibrateY()
+{
+    
+    encodey=ReadEncoder(_Y);
+    for(int x =0; x<500;x++) {
+    digitalWrite(Y_STEP_PIN,HIGH); 
+    delayMicroseconds(1000); 
+    digitalWrite(Y_STEP_PIN,LOW); 
+    delayMicroseconds(1000);
+  }
+  YStatus = 1 ;
+    while(abs(encodey-pre_encodey)>10){//){
+    digitalWrite(Y_STEP_PIN,HIGH); 
+    delayMicroseconds(500); 
+    digitalWrite(Y_STEP_PIN,LOW); 
+    delayMicroseconds(500);
+  }
+  YStatus=0;
+  digitalWrite(Y_DIR_PIN, HIGH);
+  ResetEncoder(_Y);
+  encodey=0;
+  for(int x =0; x<500;x++) {
+    digitalWrite(Y_STEP_PIN,HIGH); 
+    delayMicroseconds(1000); 
+    digitalWrite(Y_STEP_PIN,LOW); 
+    delayMicroseconds(1000);
+  } 
+  YStatus =1;
+   while( abs(encodey-pre_encodey)>5){
+    digitalWrite(Y_STEP_PIN,HIGH); 
+    delayMicroseconds(500); 
+    digitalWrite(Y_STEP_PIN,LOW); 
+    delayMicroseconds(500);
+  }
+  YStatus = 0;
+  ymax=ReadEncoder(_Y);
+//  Serial.print("Ymax");
+//  Serial.println(ymax);
+  
+}
+void calibrateZ()
+{
+    digitalWrite(Z_DIR_PIN, LOW);
+    encodez = ReadEncoder(_Z);
+    for(int x = 0; x < 500; x++) {
+        digitalWrite(Z_STEP_PIN, HIGH); 
+        delayMicroseconds(500); 
+        digitalWrite(Z_STEP_PIN, LOW); 
+        delayMicroseconds(500);
+    }
+    ZStatus = 1;
+    while (abs(encodez-pre_encodez)>200) {
+        digitalWrite(Z_STEP_PIN, HIGH); 
+        delayMicroseconds(500); 
+        digitalWrite(Z_STEP_PIN, LOW); 
+        delayMicroseconds(500);
+    }
+    ZStatus =0;
+    ResetEncoder(_Z);
+    encodez = 0;
+    digitalWrite(Z_DIR_PIN, HIGH);
+    for(int x = 0; x < 500; x++) {
+        digitalWrite(Z_STEP_PIN, HIGH); 
+        delayMicroseconds(500); 
+        digitalWrite(Z_STEP_PIN, LOW); 
+        delayMicroseconds(500);
+    }
+    ZStatus = 1;
+    while (abs(encodez-pre_encodez)>200) {
+        digitalWrite(Z_STEP_PIN, HIGH); 
+        delayMicroseconds(500); 
+        digitalWrite(Z_STEP_PIN, LOW); 
+        delayMicroseconds(500);
+    }
+    ZStatus = 0;
+    zmax=ReadEncoder(_Z);
+//    Serial.print("Zmax");
+//    Serial.println(zmax);
+}
+void calibrateX()
+{
+    digitalWrite(X1_DIR_PIN, HIGH);
+    encodex1 = ReadEncoder(_X1);
+    digitalWrite(X2_DIR_PIN, LOW);
+    encodex2 = ReadEncoder(_X2);
+    for(int x = 0; x < 1000; x++) {
+        digitalWrite(X1_STEP_PIN, HIGH); 
+        digitalWrite(X2_STEP_PIN, HIGH); 
+        delayMicroseconds(1000); 
+        digitalWrite(X1_STEP_PIN, LOW); 
+        digitalWrite(X2_STEP_PIN, LOW); 
+        delayMicroseconds(1000);
+    }
+    X1Status = 1;
+    X2Status = 1;
+    while (abs(encodex1-pre_encodex1)>30) {
+        digitalWrite(X1_STEP_PIN, HIGH); 
+        digitalWrite(X2_STEP_PIN, HIGH); 
+        delayMicroseconds(1000); 
+        digitalWrite(X1_STEP_PIN, LOW); 
+        digitalWrite(X2_STEP_PIN, LOW);
+        delayMicroseconds(1000);
+    }
+    X1Status=0;
+    X2Status=0;
+    ResetEncoder(_X1);
+    encodex1=0;
+    ResetEncoder(_X2);
+    encodex2=0;
+    digitalWrite(X1_DIR_PIN, LOW);
+    digitalWrite(X2_DIR_PIN, HIGH);
+    for(int x = 0; x < 1000; x++) {
+        digitalWrite(X1_STEP_PIN, HIGH); 
+        digitalWrite(X2_STEP_PIN, HIGH);
+        delayMicroseconds(1000); 
+        digitalWrite(X1_STEP_PIN, LOW); 
+        digitalWrite(X2_STEP_PIN, LOW);
+        delayMicroseconds(1000);
+    }
+    X1Status = 1;
+    X2Status = 1;
+    while (abs(encodex1-pre_encodex1)>30) {
+        digitalWrite(X1_STEP_PIN, HIGH); 
+        digitalWrite(X2_STEP_PIN, HIGH);
+        delayMicroseconds(1000); 
+        digitalWrite(X1_STEP_PIN, LOW); 
+        digitalWrite(X2_STEP_PIN, LOW); 
+        delayMicroseconds(1000);
+    }
+    X1Status = 0;
+    X2Status = 0;
+    x1max=-ReadEncoder(_X1);
+    x2max=ReadEncoder(_X2);
+    
+}
---
+++ encoder_interrupt_test/Move.cpp
@@ -0,0 +1,107 @@
+#include "SpiEncoder.h"
+#include "Move.h"
+void Movement(int x, int y, int z, int t)
+{
+  int ystep=1 ;
+  int x1step=1 ;
+  int x2step=1 ;
+  int zstep=1 ;
+  long Yen = ReadEncoder(_Y);
+  long X1en = -ReadEncoder(_X1);
+  long X2en = ReadEncoder(_X2);
+  long Zen = ReadEncoder(_Z);
+  adjustValue(&Yen, ymax);
+  adjustValue(&X1en, x1max);
+  adjustValue(&X2en, x2max);
+  adjustValue(&Zen, zmax);
+  int Ypos = map(Yen, 0, ymax, 0, 1280);
+  int X1pos = map(X1en, 0, x1max, 0, 1240);
+  int X2pos = map(X2en, 0, x2max, 0, 1240);
+  int Zpos = map(Zen, 0, zmax, 0, 739);
+
+  unsigned long timerXY;
+  unsigned long timerZ;
+
+  int target_ystep = abs(Ypos - y) * 10;
+  int target_x1step = abs(X1pos - x) * 10;
+  int target_x2step = abs(X2pos - x) * 10;
+  int target_zstep = abs(Zpos - z) * 25 * 2;
+
+  if (Ypos > y) {
+    digitalWrite(Y_DIR_PIN, LOW);
+  }
+  else {
+    digitalWrite(Y_DIR_PIN, HIGH);
+  }
+  if (X2pos > x) {
+
+    digitalWrite(X1_DIR_PIN, HIGH);
+    digitalWrite(X2_DIR_PIN, LOW);
+  }
+  else {
+    digitalWrite(X1_DIR_PIN, LOW);
+    digitalWrite(X2_DIR_PIN, HIGH);
+  }
+  if (Zpos > z) {
+    digitalWrite(Z_DIR_PIN, LOW);
+  }
+  else {
+    digitalWrite(Z_DIR_PIN, HIGH);
+  }
+  digitalWrite(Z_STEP_PIN, HIGH);
+  digitalWrite(Y_STEP_PIN, HIGH);
+  digitalWrite(X1_STEP_PIN, HIGH);
+  digitalWrite(X2_STEP_PIN, HIGH);
+  timerXY = micros();
+  timerZ = micros();
+  while (ystep < target_ystep || x1step < target_x1step || x2step < target_x2step || zstep < target_zstep)
+  {
+    unsigned long currentTime = micros();
+    if (currentTime - timerXY >= 1000)
+    {
+      if (ystep < target_ystep)
+      {
+        digitalWrite(Y_STEP_PIN, !digitalRead(Y_STEP_PIN));
+        ystep++;
+      }
+      if (x1step < target_x1step)
+      {
+        digitalWrite(X1_STEP_PIN, !digitalRead(X1_STEP_PIN));
+        x1step++;
+        //Serial.println(x1step);
+      }
+      if (x2step < target_x2step)
+      {
+        digitalWrite(X2_STEP_PIN, !digitalRead(X2_STEP_PIN));
+        x2step++;
+        //Serial.println(x2step);
+      }
+      timerXY = currentTime;
+    }
+    if (currentTime - timerZ >= 1000)
+    {
+      if (zstep < target_zstep)
+      {
+        digitalWrite(Z_STEP_PIN, !digitalRead(Z_STEP_PIN));
+        zstep++;
+      }
+      timerZ = currentTime;
+    }
+  }
+  target_ystep = 0 ;
+  target_x1step = 0 ;
+  target_x2step = 0;
+  target_zstep = 0 ;
+
+  ystep = 1;
+  x1step = 1;
+  x2step = 1;
+  zstep = 1;
+}
+void adjustValue(long *var, long max_var) {
+  if (*var > max_var) {
+    *var = max_var;  // Set to constant if greater
+  } else if (*var < 0) {
+    *var = 0;          // Set to 0 if negative
+  }
+}
